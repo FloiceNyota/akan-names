56ac1163fa50cd842a9a7d7b3595c38d1d2525b1
FloiceNyota98 <nyotafloice@gmail.com>
2021-04-11 15:48:20 +0300

    delete js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-let daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-let namesOfMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-let namesOfFemale =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",]
- 
-var daysOfTheWeek = function(){
-          
-}
+
