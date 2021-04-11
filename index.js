516cb4c76f477e87fa3647a7a62339833e7fc455
FloiceNyota98 <nyotafloice@gmail.com>
2021-04-11 14:51:16 +0300

    restart js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const birthdayCalculator=()=>{
-    var birthYear =document.getElementById("birthYear").value
-    var century = birthYear.slice(0,2)
-    var bYear = birthYear.slice(2,4)
-    var birthMonth = document.getElementById("birthMonth").value
-    var birthDay = document.getElementById("birthDay").value
-    var birthDate =  ( ( (century/4) -2*century-1) + ((5*bYear/4) ) + (26*(birthMonth+1)/10) + birthDay ) % 7
-    console.log(birthDate);
+let daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let namesOfMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+let namesOfFemale =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",]
+ 
+var daysOfTheWeek = function(){
+          
 }
-document.getElementById("birthdateclaculator").addEventListener("click",()=>{
-    birthdayCalculator()
-})
-const 
